02be8d1f7add43dc74073a09653eff6752e45bf2
Julien <julien.thomas84@gmail.com>
2020-11-17 15:31:55 +0100

    init firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <h1>Hello World</h1>
       </header>
     </div>
